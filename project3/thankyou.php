caefc703c2bb555c1036e3e608b0daac72ae3866
Jburgos92 <jburgos92@gmail.com>
2018-01-08 16:04:56 -0500

    pictures resized

---
+++ project3/thankyou.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Sheraton</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" href="style.css">
+
+
+
+</head>
+
+<body>
+
+    <nav class="navi">
+        <div class="photo"><img src="img/logo.png"></div>
+        <ul>
+            <li><a href="index.php">Book |</a></li>
+            <li><a href="about.php">About |</a></li>
+            <li><a href="register.php">Register |</a></li>
+            <li><a href="login.php">Login</a></li>
+
+
+
+
+
+            <?php
+			//Display the following two links only if the session variable ID exists
+			if(isset($_SESSION['uid'])){
+				echo '<li><a href="profile.php">PROFILE</a></li>';
+				echo '<li><a href="admin.php">ADMIN</a></li>';
+				echo '<li><a href="logout.php">LOGOUT</a></li>';
+			}
+		?>
+        </ul>
+    </nav>
+
+<h1>Thank you for choosing Sheraton! </h1>
+
+
+    <br>
+    <br>
+    <footer>© 2017 Sheraton, Inc., All rights reserved.</footer>
+
+    
+    
+    </body>   
+    
+    <script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+
+</html>
